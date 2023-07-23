c65a0733cd383f14925ff11fa756b2f9153625a0
Mehul Verma <mehulverma04@gmail.com>
2023-07-23 21:19:15 +0530

    minor changes

--- screenshot.py
+++ screenshot.py
@@ -1,4 +1,4 @@
-'''import pyautogui
+"""import pyautogui
 import tkinter as tk
 c=input("what do you want to enter the name of the screenshot?: ")
 root= tk.Tk()
@@ -9,17 +9,24 @@ def takeScreenshot ():
     myScreenshot.save(r'D:\\python_screenshot\\'+c+'.png')
 myButton = tk.Button(text='Take Screenshot', command=takeScreenshot, bg='green',fg='white',font= 10)
 canvas1.create_window(150, 150, window=myButton)
-root.mainloop()'''
+root.mainloop()"""
 import pyautogui
 import tkinter as tk
 from tkinter import filedialog
-root= tk.Tk()
-canvas1 = tk.Canvas(root, width = 300, height = 300)
+
+root = tk.Tk()
+canvas1 = tk.Canvas(root, width=300, height=300)
 canvas1.pack()
-def takeScreenshot ():
+
+
+def takeScreenshot():
     myScreenshot = pyautogui.screenshot()
-    file_path = filedialog.asksaveasfilename(defaultextension='.png')
+    file_path = filedialog.asksaveasfilename(defaultextension=".png")
     myScreenshot.save(file_path)
-myButton = tk.Button(text='Take Screenshot', command=takeScreenshot, bg='green',fg='white',font= 10)
+
+
+myButton = tk.Button(
+    text="Take Screenshot", command=takeScreenshot, bg="green", fg="white", font=10
+)
 canvas1.create_window(150, 150, window=myButton)
 root.mainloop()
